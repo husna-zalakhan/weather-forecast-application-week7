06d55ba7b465bb93108087f63ad706b3578908f1
husna-zalakhan <husna.zalakhan@gmail.com>
2025-06-30 02:00:53 +0000

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 function updatedWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-
   let cityElement = document.querySelector("#city");
   //moved from the handleSearchSubmit function
   // to avoid mistyping the city name
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
@@ -12,9 +14,7 @@ function searchCity(city) {
   //api call and update the interface
   //the value of the searchInput is going to send to this function
   let apiKey = "3e01514e9e60aafo05f35b074d89a4t4";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(updatedWeather);
   //get the result of api
@@ -24,10 +24,10 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city");
 
-  cityElement.innerHTML = searchInput.value;
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kabul");
